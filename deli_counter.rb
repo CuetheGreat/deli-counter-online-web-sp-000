cf0d2fd4687907b612cf5b5304b7a3910f589ea6
Clayon Stewart <c.stewart89@gmail.com>
2021-03-06 15:27:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(array)
     array.each_with_index do |person,index|
       new_array << "#{index + 1}. #{person}"
     end
-    puts new_array.join(" ")
+    puts "The line is currently: #{new_array.join(" ")}"
   else
     puts "The line is currently empty."
   end
